5eb6ba053cd09bcaea996619704082f7682ca719
HarikrishnaSiram <siramharikrishna@gmail.com>
2024-08-28 15:36:20 +0530

    removing subnet association

--- VPC/igw.tf
+++ VPC/igw.tf
@@ -4,9 +4,3 @@ resource "aws_internet_gateway" "three" {
   }
   vpc_id = aws_vpc.one.id
 }
-
-
-resource "aws_route_table_association" "two" {
-  subnet_id      = aws_subnet.two.id
-  route_table_id = aws_route_table.four.id
-}
