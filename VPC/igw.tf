427c25eb3894312b3f8fd10888d984187b6a9905
HarikrishnaSiram <siramharikrishna@gmail.com>
2024-08-28 14:43:48 +0530

    Adding route table association

--- VPC/igw.tf
+++ VPC/igw.tf
@@ -4,3 +4,9 @@ resource "aws_internet_gateway" "three" {
   }
   vpc_id = aws_vpc.one.id
 }
+
+
+resource "aws_route_table_association" "subnet1" {
+subnet_id = aws_subnet.mysubnet1.id
+route_table_id = aws_route_table.myrt.id
+}
