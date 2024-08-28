f119d78c58dee0e0674638577dfbf8b4f4fdf002
HarikrishnaSiram <siramharikrishna@gmail.com>
2024-08-28 15:46:14 +0530

    Create load_balancer.tf

---
+++ ASG & LB/load_balancer.tf
@@ -0,0 +1,16 @@
+resource "aws_elb" "myelb" {
+  name               = "my-lb"
+  security_groups    = [aws_security_group.mysg.id]
+  availability_zones = [aws_subnet.mysubnet1.availability_zone, aws_subnet.mysubnet2.availability_zone] # Changed to availability_zones
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "HTTP" # Changed to uppercase
+    lb_port           = 80
+    lb_protocol       = "HTTP" # Changed to uppercase
+  }
+
+  tags = {
+    Name = "terraform-lb"
+  }
+}
