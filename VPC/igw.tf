2923e32a2d315aacf40e975d107d2d8c4899ad4d
HarikrishnaSiram <siramharikrishna@gmail.com>
2024-08-28 14:45:20 +0530

    Update igw.tf

--- VPC/igw.tf
+++ VPC/igw.tf
@@ -7,6 +7,6 @@ resource "aws_internet_gateway" "three" {
 
 
 resource "aws_route_table_association" "two" {
-  subnet_id      = aws_subnet.mysubnet1.id
+  subnet_id      = aws_subnet.two.id
   route_table_id = aws_route_table.myrt.id
 }
